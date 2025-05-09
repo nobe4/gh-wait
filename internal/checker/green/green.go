2170ea19e2f3e146f1bdc922553ff466c8bc3b14
nobe4 <nobe4@users.noreply.github.com>
2025-05-09 08:22:19 +0200

    feat(green): format output and simplify code

--- internal/checker/green/green.go
+++ internal/checker/green/green.go
@@ -4,66 +4,43 @@ import (
 	"fmt"
 
 	graphql "github.com/cli/shurcooL-graphql"
+
 	"github.com/nobe4/gh-wait/internal/github"
+)
+
+const (
+	// https://docs.github.com/en/graphql/reference/enums#checkconclusionstate
+	// Longest value is 15 characters.
+	maxStatusLength = 15
+
+	// https://docs.github.com/en/graphql/reference/enums#checkstatusstate
+	// Longest value is 11 characters.
+	maxConclusionLength = 11
+
+	// TODO: get terminal width.
+	// 80 - 1 - 15 - 1 - 11 = 52.
+	maxNameLength = 52
 )
 
 type Checker struct{}
 
-type Repo struct {
-	Pull Pull `json:"pullRequest"`
-}
-
-type Pull struct {
-	StatusCheckRollup StatusCheckRollup `json:"statusCheckRollup"`
-}
-
-type StatusCheckRollup struct {
-	Contexts Contexts `json:"contexts"`
-}
-
-type Contexts struct {
-	Nodes []CheckRun `json:"nodes"`
-}
-
 type CheckRun struct {
-	Name       string `json:"name"`
-	Status     string `json:"status"`
-	Conclusion string `json:"conclusion"`
+	Name       string
+	Status     string
+	Conclusion string
 }
 
-const query = `
-query pullChecks($owner: String!, $repo: String!, $number: Int!){
-  repository(owner: $owner, name: $repo){
-    pullRequest(number: $number){
-      statusCheckRollup {
-        contexts(first: 100) {
-          nodes{
-            ...on CheckRun{
-              name,
-              status,
-              conclusion,
-            }
-          }
-        }
-      }
-    }
-  }
-}
-`
+type CheckRuns []CheckRun
 
 func (Checker) Check(p github.Pull) (bool, string, error) {
-	//nolint:revive // Allow nested structure for simplicity
+	//revive:disable:nested-structs // Allow nested structure for simplicity
 	var query struct {
 		Repository struct {
 			PullRequest struct {
 				StatusCheckRollup struct {
 					Contexts struct {
 						Nodes []struct {
-							CheckRun struct {
-								Name       string
-								Status     string
-								Conclusion string
-							} `graphql:"...on CheckRun"`
+							CheckRun CheckRun `graphql:"...on CheckRun"`
 						}
 					} `graphql:"contexts(first: 100)"`
 				}
@@ -78,14 +55,66 @@ func (Checker) Check(p github.Pull) (bool, string, error) {
 	}
 
 	if err := github.GraphQL(&query, variables); err != nil {
-		return false, "TODO", err
+		return false, "", err //nolint:wrapcheck // Allow github's errors
 	}
 
-	checks := query.Repository.PullRequest.StatusCheckRollup.Contexts.Nodes
+	checks := CheckRuns{}
 
-	for _, check := range checks {
-		fmt.Printf("%+v\n", check.CheckRun)
+	green := true
+
+	for _, node := range query.Repository.PullRequest.StatusCheckRollup.Contexts.Nodes {
+		checks = append(checks, node.CheckRun)
+
+		if node.CheckRun.Status != "COMPLETED" || node.CheckRun.Conclusion != "SUCCESS" {
+			green = false
+		}
 	}
 
-	return false, "TODO", nil
+	out := checks.String()
+
+	if green {
+		out += "All checks have completed successfully"
+	} else {
+		out += "Some checks are still needed"
+	}
+
+	return green, out, nil
+}
+
+//revive:disable:cognitive-complexity // This doesn't need to be simplified.
+func (r CheckRuns) String() string {
+	nameLength := 0
+	statusLength := 0
+	conclusionLength := 0
+
+	for _, check := range r {
+		if l := len(check.Name); l > nameLength {
+			nameLength = l
+		}
+
+		if l := len(check.Status); l > statusLength {
+			statusLength = l
+		}
+
+		if l := len(check.Conclusion); l > conclusionLength {
+			conclusionLength = l
+		}
+	}
+
+	nameLength = min(nameLength, maxNameLength)
+	statusLength = min(statusLength, maxStatusLength)
+	conclusionLength = min(conclusionLength, maxConclusionLength)
+
+	out := ""
+
+	for _, check := range r {
+		out += fmt.Sprintf(
+			"%-*s %-*s %-*s\n",
+			nameLength, check.Name[:min(nameLength, len(check.Name))],
+			statusLength, check.Status,
+			conclusionLength, check.Conclusion,
+		)
+	}
+
+	return out
 }
